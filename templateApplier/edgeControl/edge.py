6b389c77de5906f7d69ec4e62b52c6d454569a69
EGAP\202212002 <202212002@egap.co.kr>
2023-01-11 17:23:20 +0900

    edgeDelete

--- templateApplier/edgeControl/edge.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-edge_options = webdriver.EdgeOptions()
-driver = webdriver.ChromiumEdge("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", options=edge_options)
-
-driver.get("www.google.com")
-driver.close()
-quit()
